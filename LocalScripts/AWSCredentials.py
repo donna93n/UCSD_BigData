975fe908c95d689184b1704518bb2d6c46983c3f
donna93n <donna.93n@gmail.com>
2014-04-09 15:03:50 -0700

    changed credentials file

--- LocalScripts/AWSCredentials.py
+++ LocalScripts/AWSCredentials.py
@@ -2,12 +2,12 @@
 # Change entries here to match your own #
 # Get values for the first two entries from the "Security crediential" Tab
 # Under in the menu under your name.
-aws_access_key_id='Key ID'
-aws_secret_access_key='Key'
+aws_access_key_id='AKIAIXKAXYLKA6OZYK6A'
+aws_secret_access_key='v0KFO/0dQVu0qKwThhL/AgKZhWUsZgoK8rH6pBIG'
 # Get the Keypair in the EC2 Dashboard page.
-keyPairFile="<~/.ssh/KeyFilename.pem>" # name of file keeping local key
-key_name="key name" # name of keypair (not name of file where key is stored)
+keyPairFile="</c/Users/Donna\ Nguyen/.ssh/virginiaKeypair.pem>" # name of file keeping local key
+key_name="virginiaKeypair" # name of keypair (not name of file where key is stored)
 # Set the security group On the EC2 page (You will need to add IP addresses if
 # you want to connect from a place previously unauthorized.
-security_groups=['GroupName']
+security_groups=['291']
 ### End of AWS credentials ####
